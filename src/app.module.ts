ff29d4d14a566c477c6919a377b0425a670c1556
Habeeb Rahman <habeebrahmanca07@gmail.com>
2025-02-17 14:35:03 +0530

    added redis store to nesths

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { StudentsModule } from './students/students.module';
+import { redisStore } from 'cache-manager-redis-yet';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
     CacheModule.register({
       isGlobal: true,
-      ttl: 30 * 1000,
+      imports: [ConfigModule],
+      useFactory: async (config) => {
+        const store = await redisStore({
+          ttl: 30 * 1000,
+          socket: {
+            host: config.get('redis.host'),
+            port: config.get('redis.port'),
+          },
+        });
+        return { store };
+      },
+      inject: [ConfigService],
     }),
     StudentsModule,
   ],
--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { StudentsService } from './students.service';
-import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
 
 @UseInterceptors(CacheInterceptor)
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
-  @CacheTTL(60 * 1000)
+  @CacheTTL(0)
+  @CacheKey('MYKEY')
   @Get()
   async getStudents() {
     return this.studentsService.getStudents();
